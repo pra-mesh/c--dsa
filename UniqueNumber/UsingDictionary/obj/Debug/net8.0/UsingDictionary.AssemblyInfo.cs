f7234e067c2fffafc2cb0ce4a6f30ba8a43570fc
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2024-10-03 21:48:36 +0545

    rumUsingDic

--- UniqueNumber/UsingDictionary/obj/Debug/net8.0/UsingDictionary.AssemblyInfo.cs
+++ UniqueNumber/UsingDictionary/obj/Debug/net8.0/UsingDictionary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b002357ba1be7894a24302a0ca0e7d7b7d46a44d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4056afd94e222db32f89f79ec43e97b2223d2d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
