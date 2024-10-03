3cc8250b5b0e9fe3a93f60af3461dd57a889dd0f
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2024-10-03 22:04:26 +0545

    Unique number using simple loop

---
+++ UniqueNumber/simpleloop/Program.cs
@@ -0,0 +1,21 @@
+﻿  int[] inputs =  [4, 3, 2, 4, 1, 3, 2, 7,7,8];
+  List<int> uniqueNumbers = new List<int>();
+  for (int i = 0; i < inputs.Length; i++)
+  {
+    int count = 0;
+    for (int j = 0; j< inputs.Length; j++)
+    {
+      if(inputs[i] == inputs[j])
+      {
+        count++;
+      }
+      if(count>1)
+      {
+        break;
+      }
+    }
+    if(count==1){
+      uniqueNumbers.Add(inputs[i]);
+    }
+  }
+  System.Console.WriteLine($"[{string.Join(", ", uniqueNumbers)}]");
