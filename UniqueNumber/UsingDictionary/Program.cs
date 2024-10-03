f4056afd94e222db32f89f79ec43e97b2223d2d0
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2024-10-03 21:40:22 +0545

    unique number using dictionary

---
+++ UniqueNumber/UsingDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    int[] inputs =  [4, 3, 2, 4, 1, 3, 2, 7];
+    int[] outputs = FindUniqueNumber(inputs);
+    System.Console.WriteLine($"[{string.Join(", ", outputs)}]");
+  }
+  static int[] FindUniqueNumber(int[] inputs)
+  {
+    Dictionary<int,int> countNumber = new ();
+     foreach (int input in inputs)
+     {
+      if(countNumber.ContainsKey(input))
+      {
+        countNumber[input]++;
+      }
+      else
+      {
+        countNumber.Add(input, 1);
+        //countNumber[input] =1;
+      }
+     }
+     //using linq
+     var value = countNumber.Where(x => x.Value == 1).Select(g =>g.Key).ToArray();
+     //using loop
+     List<int> uniqueElements = new List<int>();
+     foreach (var number in countNumber)
+     {
+      if(number.Value == 1){
+        uniqueElements.Add(number.Key);
+      }
+     }
+     return uniqueElements.ToArray();
+  }
+}
