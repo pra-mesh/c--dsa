bfdc6f015b09a7e96f646ca81db47f5b19ca1204
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2024-10-05 08:25:07 +0545

    odd even using list

---
+++ OddEvenSeparate/Usinglist/Program.cs
@@ -0,0 +1,42 @@
+﻿int [] inputs = [1, 2, 4, 5, 10, 6, 3, 51];
+List<int> evens = new List<int>();
+List<int> odds = new List<int>();
+
+evens = GetEvenNumber(inputs);
+odds = inputs.Except(evens).ToList();
+System.Console.WriteLine($"Even Numbers are [{string.Join(", ", evens)}]");
+System.Console.WriteLine($"Odd Numvers are [{string.Join(", ", odds)}]");
+static List<int> GetEvenNumber(int [] inputs)
+{
+  List<int> evenNumbers = new ();
+   foreach (int i in inputs){
+    if(i%2==0)
+      evenNumbers.Add(i);
+   }
+
+   return evenNumbers;
+}
+
+//uing tuple
+//var v = GetOddEven(inputs);
+(List<int> odds, List<int> evens) v = GetOddEven(inputs);
+
+evens = v.Item2;
+odds = v.Item1;
+System.Console.WriteLine($"Even Numbers are [{string.Join(", ", evens)}]");
+System.Console.WriteLine($"Odd Numvers are [{string.Join(", ", odds)}]");
+
+static (List<int> odd, List<int> even) GetOddEven(int[] inputs)
+{
+  List<int> oddNumbers = new ();
+  List<int> evenNumbers = new ();
+  for (int i = 0;i<inputs.Count();i++){
+    if(inputs[i]%2 ==0){
+      evenNumbers.Add(inputs[i]);
+    }
+    else
+      oddNumbers.Add(inputs[i]);
+     
+  }
+   return(oddNumbers,evenNumbers);
+}
--- UniqueNumber/UsingDictionary/obj/Debug/net8.0/UsingDictionary.AssemblyInfo.cs
+++ UniqueNumber/UsingDictionary/obj/Debug/net8.0/UsingDictionary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff0c48207511340ee90c841e1435d77d3f108611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d919db0c499f28c5eeb3e4f6f74e874a1f802fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsingDictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
